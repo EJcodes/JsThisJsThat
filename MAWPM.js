7db31190a8c4207f21cd86ad12de636a7d04460a
Ejcodes <ihmdemon.exe@gmail.com>
2023-10-31 17:07:56 -0400

    new problem

--- MAWPM.js
+++ MAWPM.js
@@ -300,3 +300,19 @@ let testArr = [1, 2, 3, 4, 5];
 console.log("Before: " + JSON.stringify(testArr));
 console.log(nextInLine(testArr, 6));
 console.log("After: " + JSON.stringify(testArr));
+
+/*
+Another data type is the Boolean. Booleans may only be one of two values: true or false. They are basically little on-off switches, where true is on and false is off. These two states are mutually exclusive.
+
+Note: Boolean values are never written with quotes. The strings "true" and "false" are not Boolean and have no special meaning in JavaScript.
+
+Modify the welcomeToBooleans function so that it returns true instead of false.
+*/
+
+function welcomeToBooleans() {
+  // Only change code below this line
+  
+  return false; // Change this line
+
+  // Only change code above this line
+}
