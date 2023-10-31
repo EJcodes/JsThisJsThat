3645324da43b5da4a2c2217864cb613a92ca3364
Ejcodes <ihmdemon.exe@gmail.com>
2023-10-31 17:09:40 -0400

    problem with solution

--- MAWPM.js
+++ MAWPM.js
@@ -312,7 +312,8 @@ Modify the welcomeToBooleans function so that it returns true instead of false.
 function welcomeToBooleans() {
   // Only change code below this line
   
-  return false; // Change this line
+  return true; // Change this line
+  
 
   // Only change code above this line
 }
