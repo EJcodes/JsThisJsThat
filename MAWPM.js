72c10f6f572e2cf50a3ec1f8990354489ec6b112
Ejcodes <ihmdemon.exe@gmail.com>
2023-10-31 17:11:45 -0400

    New question

--- MAWPM.js
+++ MAWPM.js
@@ -313,7 +313,38 @@ function welcomeToBooleans() {
   // Only change code below this line
   
   return true; // Change this line
-  
 
   // Only change code above this line
+}
+
+/*
+
+if statements are used to make decisions in code. The keyword if tells JavaScript to execute the code in the curly braces under certain conditions, defined in the parentheses. These conditions are known as Boolean conditions and they may only be true or false.
+
+When the condition evaluates to true, the program executes the statement inside the curly braces. When the Boolean condition evaluates to false, the statement inside the curly braces will not execute.
+
+Pseudocode
+
+if (condition is true) {
+  statement is executed
+}
+Example 
+function test(myCondition) {
+  if (myCondition) {
+    return "It was true";
+  }
+  return "It was false";
+}
+
+test(true);
+test(false);
+*/
+
+function trueOrFalse(wasThatTrue) {
+  // Only change code below this line
+  
+
+  
+  // Only change code above this line
+
 }
